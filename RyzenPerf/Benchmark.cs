51788b5ebe11cef1f164ed72ca94f387408753c0
Ben Owen <LordBenjamin@users.noreply.github.com>
2019-06-29 22:02:31 +0100

    Initial commit

---
+++ RyzenPerf/Benchmark.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Buffers;
+using System.IO;
+using System.Runtime.InteropServices;
+using BenchmarkDotNet.Attributes;
+
+namespace RyzenPerf
+{
+    [CoreJob]
+    [ClrJob]
+    public class Benchmark
+    {
+        private const int SharedArrayPoolMaxBufferSize = 1024 * 1024;
+        private const int SourceDataLength = 3000000;
+
+        private Stream stream = new MemoryStream();
+
+        private IntPtr unmanagedBuffer;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            unmanagedBuffer = Marshal.AllocHGlobal(SourceDataLength);
+        }
+
+        [GlobalCleanup]
+        public void Cleanup()
+        {
+            Marshal.FreeHGlobal(unmanagedBuffer);
+        }
+
+        [Benchmark]
+        public void Run()
+        {
+            stream.Seek(0, SeekOrigin.Begin);
+            StreamWrite(unmanagedBuffer, SourceDataLength);
+        }
+
+        // This method runs faster under .NET Framework than .NET Core on an AMD Ryzen 1200.
+        // The expectation is that .NET Core should be faster, given the focus on performance,
+        // runtime support for Span<T>, etc. This is true on an Intel i7 6700.
+        public unsafe uint StreamWrite(IntPtr buffer, uint size)
+        {
+            var arrayPool = ArrayPool<byte>.Shared;
+            byte[] managedBuffer = arrayPool.Rent(
+                size < SharedArrayPoolMaxBufferSize ? (int)size : SharedArrayPoolMaxBufferSize);
+
+            int writeSize = (int)Math.Min(managedBuffer.Length, size);
+            uint numWrites = (uint)Math.Ceiling(size / (float)writeSize);
+
+            byte* ptr = (byte*)buffer.ToPointer();
+            uint writeCount = 0;
+
+            int bytesWritten = 0;
+
+            try
+            {
+                while (writeCount < numWrites)
+                {
+                    int remainder = (int)((size - bytesWritten) % writeSize);
+                    int actualWriteSize = remainder > 0 ? remainder : writeSize;
+
+                    ReadOnlySpan<byte> source = new ReadOnlySpan<byte>(ptr, actualWriteSize);
+                    ptr += actualWriteSize;
+
+                    source.CopyTo(managedBuffer);
+
+                    // Commenting this line out makes the .NET Core benchmark run faster than Framework
+                    // on a Ryzen 1200 CPU
+                    stream.Write(managedBuffer, 0, actualWriteSize);
+
+                    writeCount++;
+                    bytesWritten += actualWriteSize;
+                }
+
+                return writeCount;
+            }
+            finally
+            {
+                arrayPool.Return(managedBuffer);
+            }
+        }
+    }
+
+}
---
+++ RyzenPerf/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace RyzenPerf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Benchmark>();
+        }
+    }
+}
